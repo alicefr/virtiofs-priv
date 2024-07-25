3ebfc369aa9f2653a0651132fc41217ad218fb34
Alice Frosi <afrosi@redhat.com>
2024-07-25 15:05:25 +0200

    Replace the demo with the virtiofs mock

--- demo.sh
+++ demo.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -xe
 
 podman run --rm -ti --name demo \
-	--user 1000 \
+	--user test \
+	 -w /home/test \
 	--security-opt=seccomp=demo.json \
 	--annotation run.oci.seccomp.receiver=/tmp/demo.sock \
-	busybox \
-	sh -c "touch /tmp/blabla && chown 1000:1000 /tmp/blabla"
+	vfsd-mock:latest \
+	/usr/local/bin/vfsd-mock --shared-dir /home/test/share-dir  --file /home/test/share-dir/demo
+#	busybox \
+#	sh -c "touch /tmp/blabla && chown 1000:1000 /tmp/blabla"
