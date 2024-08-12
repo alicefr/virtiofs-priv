174a9a0088f10c13a8aca252e2b4dd4b55408ee1
Alice Frosi <afrosi@redhat.com>
2024-08-12 17:46:08 +0200

    mock: try to write in the open file

--- src/vfsd-mock.rs
+++ src/vfsd-mock.rs
@@ -1,13 +1,14 @@
+use crate::filehandle::{open_by_handle_at, FileHandle};
 use clap::Parser;
+use oslib::openat;
 use std::fs::File;
+use std::io::Write;
 use std::os::fd::AsRawFd;
 use std::path::PathBuf;
 use std::{fs, io};
-use oslib::openat;
-use crate::filehandle::{FileHandle, open_by_handle_at};
 
-mod oslib;
 mod filehandle;
+mod oslib;
 
 #[derive(Parser)]
 struct Cli {
@@ -42,8 +43,9 @@ fn main() {
         FileHandle::from_fd(&file_fd).expect("name_to_handle_at")
     };
     println!("\n(name_to_handle_at) received FH: {:?}\n", file_fh);
-
-    let f = open_by_handle_at(&root_dir_fd, &file_fh.handle, 0).expect("open_by_handle_at");
+    let flags = libc::O_CREAT | libc::O_APPEND | libc::O_RDWR;
+    let mut f = open_by_handle_at(&root_dir_fd, &file_fh.handle, flags).expect("open_by_handle_at");
     println!("\n(open_by_handle_at) received FD: {}\n", f.as_raw_fd());
+    write!(&mut f, "It works!").expect("failed to write");
     println!("it works!");
 }
