bad889b33d90c7b50e9093c17752545a83f6db24
Alice Frosi <afrosi@redhat.com>
2024-11-04 13:33:23 +0100

    Fix vfsd demo script

--- demo-vfsd.sh
+++ demo-vfsd.sh
@@ -6,12 +6,11 @@ podman run --rm -ti --name demo \
 	--user test \
 	--security-opt label=disable  \
 	--volume $(pwd)/${VFSDWD}/shared-dir:/shared-dir:U \
-	--security-opt=seccomp=demo.json \
+	--security-opt=seccomp=$(pwd)/demo.json \
 	--annotation run.oci.seccomp.receiver=/tmp/demo.sock \
         --volume $(pwd)/${VFSDWD}/socket:/socket:U \
         vfsd:latest \
-        /bin/bash
-        #/usr/local/bin/virtiofsd --socket-path=/socket/vsfd.sock --shared-dir=/shared-dir --sandbox=none --log-level=debug --inode-file-handles=mandatory
+        /usr/local/bin/virtiofsd --socket-path=/socket/vsfd.sock --shared-dir=/shared-dir --sandbox=none --log-level=debug --inode-file-handles=mandatory
 
 
 
